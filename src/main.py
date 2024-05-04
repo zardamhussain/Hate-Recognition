bae49680b2e0a2a2b59d26d5af9178cab8bbbd09
zardam <zardamhussain@gmail.com>
2024-05-04 14:37:57 +0530

    fixed

--- src/main.py
+++ src/main.py
@@ -1,37 +1,25 @@
-from appwrite.client import Client
+import pickle
 import os
 
+__dirname = os.path.dirname(os.path.abspath(__file__))
 
-# This is your Appwrite function
-# It's executed each time we get a request
-def main(context):
-    # Why not try the Appwrite SDK?
-    #
-    # client = (
-    #     Client()
-    #     .set_endpoint("https://cloud.appwrite.io/v1")
-    #     .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
-    #     .set_key(os.environ["APPWRITE_API_KEY"])
-    # )
+model = pickle.load(open(os.path.join(__dirname, 'hateClassify.pkl'), 'rb'))
+vector = pickle.load(open(os.path.join(__dirname, 'vectorized.pkl', 'rb')))
 
-    # You can log messages to the console
-    context.log("Hello, Logs!")
+def main(context):    
 
-    # If something goes wrong, log an error
-    context.error("Hello, Errors!")
+    return context.res.json(context.req)
 
-    # The `ctx.req` object contains the request data
-    if context.req.method == "GET":
-        # Send a response with the res object helpers
-        # `ctx.res.send()` dispatches a string back to the client
-        return context.res.send("Hello, World!")
+    # text = context.req.params.get('text')
 
-    # `ctx.res.json()` is a handy helper for sending JSON
-    return context.res.json(
-        {
-            "motto": "Build like a team of hundreds_",
-            "learn": "https://appwrite.io/docs",
-            "connect": "https://appwrite.io/discord",
-            "getInspired": "https://builtwith.appwrite.io",
-        }
-    )
+    # if text is None:
+    #     context.response.send('Error: "text" query parameter is missing.', status_code=400)
+    #     return
+    
+
+    # res = bool(model.predict(vector.transform([text]))[0]) 
+
+    # context.re
+
+    # return context.res.send("Hello, World!")
+
